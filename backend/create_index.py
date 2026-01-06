9a23c3aeadf63f27cb32492fb23d34e8161765fa
Faisal-khann <faisalkhanfaisal496@gmail.com>
2026-01-07 00:26:47 +0530

    define the data path

--- backend/create_index.py
+++ backend/create_index.py
@@ -3,7 +3,18 @@
 #embeddings 
 #store in vector store -> FAISS
 
-from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader, TextLoader
+import os
+from langchain_community.document_loaders import (
+    DirectoryLoader,
+    PyPDFLoader,
+    TextLoader
+)
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
+
+# Root folder containing all knowledge documents (PDF + TXT)
+DATA_PATH = "../knowledge_base"
+
+# Folder where FAISS index + metadata will be persisted
+FAISS_PATH = "../faiss_index"
