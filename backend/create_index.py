d48422208ef1664c4d498cce33f99241967f119d
Faisal-khann <faisalkhanfaisal496@gmail.com>
2026-01-06 23:40:50 +0530

    create RAG flow

--- backend/create_index.py
+++ backend/create_index.py
@@ -1,1 +1,9 @@
-# 
+#read files
+#break in chunks
+#embeddings 
+#store in vector store -> FAISS
+
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader, TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
